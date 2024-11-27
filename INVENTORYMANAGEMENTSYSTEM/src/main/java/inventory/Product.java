5ef28aabde9987c8f6e35d81bec52bd012c48c88
emirhandumann <emirdumanhan@gmail.com>
2024-11-27 15:17:09 +0300

    product.java is implemented

---
+++ INVENTORYMANAGEMENTSYSTEM/src/main/java/inventory/Product.java
@@ -0,0 +1,55 @@
+package main.java.inventory;
+
+import java.time.LocalDate;
+
+public class Product {
+    private String name;
+    private int stock;
+    private double price;
+    private String category;
+    private LocalDate expiryDate;
+
+    public Product(String name, int stock, double price, String category, LocalDate expiryDate) {
+        this.name = name;
+        this.stock = stock;
+        this.price = price;
+        this.category = category;
+        this.expiryDate = expiryDate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public LocalDate getExpiryDate() {
+        return expiryDate;
+    }
+
+    public void setExpiryDate(LocalDate expiryDate) {
+        this.expiryDate = expiryDate;
+    }
+}
