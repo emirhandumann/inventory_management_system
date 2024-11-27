dfbf1e0a5decf3f9d365a1002292acc206ac9a1c
emirhandumann <emirdumanhan@gmail.com>
2024-11-27 16:38:14 +0300

    InventoryApp.java is implemented

---
+++ INVENTORYMANAGEMENTSYSTEM/src/main/java/inventory/InventoryApp.java
@@ -0,0 +1,10 @@
+package main.java.inventory;
+
+public class InventoryApp {
+
+    public static void main(String[] args) {
+        // Create an instance of InventoryMenu and start the terminal interface
+        InventoryMenu menu = new InventoryMenu();
+        menu.start();
+    }
+}
